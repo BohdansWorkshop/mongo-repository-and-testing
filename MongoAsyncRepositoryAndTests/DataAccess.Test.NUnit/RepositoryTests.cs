1ba2293d33fcebb21fdf1185d67ec168b263d547
Bohdan <universalshon@gmail.com>
2019-10-31 22:57:32 +0200

    removed unnecessary project moved setup in test to right place

--- MongoAsyncRepositoryAndTests/DataAccess.Test.NUnit/RepositoryTests.cs
+++ MongoAsyncRepositoryAndTests/DataAccess.Test.NUnit/RepositoryTests.cs
@@ -14,31 +14,34 @@ namespace DataAccess.Test.NUnit
     public class RepositoryTests
     {
         List<TextDataModel> _mockData;
+        public Mock<IMongoRepository<TextDataModel>> _mockRepository;
+
         [SetUp]
         public void Setup()
         {
             _mockData = GenerateFakeData();
-        }
 
-        public Mock<IMongoRepository<TextDataModel>> _mockRepository;
-        public RepositoryTests()
-        {
             _mockRepository = new Mock<IMongoRepository<TextDataModel>>();
 
             _mockRepository.Setup(x => x.FindAll()).Returns(_mockData);
-            _mockRepository.Setup(x => x.GetById(It.IsAny<Guid>())).Returns((Guid id) => { return _mockData.Where(x => x.Id == id).FirstOrDefault(); });
 
+            _mockRepository.Setup(x => x.GetById(It.IsAny<Guid>())).Returns((Guid id) => { return _mockData.Where(x => x.Id == id).FirstOrDefault(); });
             _mockRepository.Setup(x => x.GetPage(It.IsAny<Guid>(), It.IsAny<int>())).Returns((Guid id, int takeCount) => {
                 var currentItem = _mockData.FirstOrDefault(x => x.Id == id);
-                var indexOfItem = _mockData.IndexOf(currentItem); 
-                var pagedList = _mockData.Where(x=> _mockData.IndexOf(x) >= indexOfItem ).Take(takeCount).ToList();
+                var indexOfItem = _mockData.IndexOf(currentItem);
+                var pagedList = _mockData.Where(x => _mockData.IndexOf(x) >= indexOfItem).Take(takeCount).ToList();
                 return pagedList;
             });
-
         }
 
         [Test]
-        public void IsFirstObjectsEqual()
+        public void DoesReturnRightCollection()
+        {
+            Assert.AreEqual(_mockRepository.Object.FindAll().ToList(), _mockData);
+        }
+
+        [Test]
+        public void IsGetByIdWorks()
         {
             Assert.AreEqual(_mockRepository.Object.GetById(_mockData.FirstOrDefault().Id), _mockData.FirstOrDefault());
         }
